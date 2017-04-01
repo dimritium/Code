78e489538efc0c0b0507c059956fbb0e679f42ca
shubhiD <dimrishubhi@gmail.com>
2017-04-01 20:15:10 +0530

    1-4

--- Algos/knightTour.cpp
+++ Algos/knightTour.cpp
@@ -2,15 +2,9 @@
 #define fl(a,b,c) for(a=b;a<c;a++)
 #define MOD 1000000007
 #define pb push_back
-#define N 8
+
 using namespace std;
 
-int solveKTUtil(int x, int y, int movei, int sol[N][N], int xMove[], int yMove[]);
-
-bool isSafe(int x, int y, int sol[N][N])
-{
-	return (x>=0 && x<N && y>0 && y<N && sol[x][y] == -1);
-}
 int main()
 {
 	this;
